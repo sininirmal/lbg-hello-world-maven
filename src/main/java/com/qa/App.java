930348078bbc32ec9715e145aea1d1771817e2fa
Sini <you@example.com>
2024-09-03 14:11:38 +0100

    corrected error

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,7 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
-        System.out.println(SayHelloToSomeOne("Sini"))
+        System.out.println(SayHelloToSomeOne("Sini"));
         System.out.println(sayGoodbye());
     }
 
