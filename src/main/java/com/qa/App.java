47f8d3288a426ee2208c8d878312585ab9c0536c
Sini <you@example.com>
2024-09-03 14:17:26 +0100

    added lines

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,6 +13,8 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
+         System.out.println(sayHelloToSomeone("Sini"));
+        System.out.println(sayHelloToSomeone("Arpita"));
         System.out.println(sayGoodbye());
     }
 
